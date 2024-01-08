d37f1baaa8e4f1d51bc9e962ee084b6dfa8cf02e
chathushkaf <chathushkaf65@gmail.com>
2024-01-08 14:09:56 +0530

    commit

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "chathushkaf-tfstate-bkt" #change this
     key    = "terraform.tfstate"
     region = "us-east-1"
-    #dynamodb_table = "my-lock-table" # optional (Only if you created the DynamoDB table in step 4) 
+    dynamodb_table = "my-lock-table" # optional (Only if you created the DynamoDB table in step 4) 
   }
 }
 
